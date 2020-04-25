80f9d9b16e08512fafd3b4e31cbf32ac7ba4aa74
Jordan Martin <jordantmartin.dev@gmail.com>
2020-04-24 21:58:08 -0400

    Clean up target files

--- sdl-game/src/target.cpp
+++ sdl-game/src/target.cpp
@@ -3,7 +3,7 @@
 Target::Target(SDL_Renderer* renderer, int w, int h, int x, int y, const std::string& image_path) 
 	: Rect(renderer, w, h, x, y, image_path)
 {
-	health_text = new Text(renderer, "assets/consola.ttf", 30, std::to_string(health), { 255, 0, 0 , 255 }, _x + 5, _y - 50);
+	health_text = new Text(renderer, FONT_PATH, FONT_SIZE, std::to_string(health), green, _x + 5, _y - 50);
 }
 
 Target::~Target() {
@@ -11,17 +11,17 @@ Target::~Target() {
 }
 
 void Target::update(SDL_Renderer* renderer) {
-	health_text->reloadTexture(renderer, "assets/consola.ttf", 30, std::to_string(health), { 0, 128, 0 , 255 });
+	health_text->reloadTexture(renderer, FONT_PATH, FONT_SIZE, std::to_string(health), green);
 	health_text->draw(renderer);
 }
 
 void Target::hit() { 
-	printf("Target hit!\n");
 	health -= 10;
-	printf("Health is now %d \n", health);
 }
 
 bool Target::isDead() {
-	if (health <= 0) { return true; }
-	else return false;
+	if (health <= 0) 
+		return true; 
+
+	return false;
 }
--- sdl-game/src/target.h
+++ sdl-game/src/target.h
@@ -1,8 +1,6 @@
 #pragma once
 #include "rect.h"
 #include "text.h"
-#include <vector>
-#include <string>
 #include <map>
 
 
@@ -10,9 +8,18 @@ class Target : public Rect {
 public:
 	Target(SDL_Renderer* renderer, int w, int h, int x, int y, const std::string& image_path);
 	~Target();
+
 	void update(SDL_Renderer* renderer);
 	void hit();
 	bool isDead();
+
+
+private:
 	int health = 100;
 	Text* health_text;
+
+	// Constants for text
+	const std::string FONT_PATH = "assets/consola.ttf";
+	const int FONT_SIZE = 30;
+	const SDL_Color green{ 0, 128, 0 , 255 };
 };

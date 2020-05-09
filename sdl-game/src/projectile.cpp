064e558cb737128e096f59034def0e3393a83d45
Jordan Martin <jordantmartin.dev@gmail.com>
2020-05-09 17:01:58 -0400

    Moved functions around

--- sdl-game/src/projectile.cpp
+++ sdl-game/src/projectile.cpp
@@ -16,6 +16,100 @@ Projectile::~Projectile() {
 
 }
 
+std::map<int, points> Projectile::setupTargetLocations() {
+	std::map<int, points> locations;
+	points p;
+
+	p.point1 = std::make_pair(0, 30);
+	p.point2 = std::make_pair(700, 30);
+	locations[0] = p;
+
+	p.point1 = std::make_pair(0, 100);
+	p.point2 = std::make_pair(0, 200);
+	locations[1] = p;
+
+	p.point1 = std::make_pair(100, 100);
+	p.point2 = std::make_pair(100, 200);
+	locations[2] = p;
+
+	p.point1 = std::make_pair(200, 130);
+	p.point2 = std::make_pair(570, 130);
+	locations[3] = p;
+
+	p.point1 = std::make_pair(200, 230);
+	p.point2 = std::make_pair(570, 230);
+	locations[4] = p;
+
+	p.point1 = std::make_pair(650, 100);
+	p.point2 = std::make_pair(650, 200);
+	locations[5] = p;
+
+	p.point1 = std::make_pair(750, 100);
+	p.point2 = std::make_pair(750, 200);
+	locations[6] = p;
+	return locations;
+}
+
+std::map<int, std::pair<int, int>> Projectile::setupObstLocations() {
+	std::map<int, std::pair<int, int> > locations;
+	locations[0] = std::make_pair(50, 300);
+	locations[1] = std::make_pair(375, 325);
+	locations[2] = std::make_pair(550, 300);
+	locations[3] = std::make_pair(200, 350);
+	locations[4] = std::make_pair(700, 325);
+	locations[5] = std::make_pair(400, 400);
+	locations[6] = std::make_pair(600, 400);
+	locations[7] = std::make_pair(175, 400);
+	return locations;
+}
+
+std::vector<std::shared_ptr<Target>> Projectile::createTargets(SDL_Renderer* renderer) {
+	std::vector<std::shared_ptr<Target>> targets;
+	for (size_t i = 0; i < _target_locations.size(); i++) {
+		std::shared_ptr<Target> target(new Target(renderer, TARGET_SIZE, TARGET_SIZE, _target_locations[i].point1.first, _target_locations[i].point1.second, _target_locations[i].point2.first, _target_locations[i].point2.second, TARGET_FILE_PATH));
+		targets.push_back(target);
+	}
+	return targets;
+}
+
+std::vector<std::shared_ptr<Rect>> Projectile::createObstacles(SDL_Renderer* renderer) {
+	std::vector<std::shared_ptr<Rect>> obstacles;
+	for (size_t i = 0; i < _obst_locations.size(); i++) {
+		std::shared_ptr<Rect> obstacle(new Rect(renderer, ROCK_W, ROCK_H, _obst_locations[i].first, _obst_locations[i].second, ROCK_FILE_PATH));
+		obstacles.push_back(obstacle);
+	}
+	return obstacles;
+}
+
+void Projectile::createProjectile(SDL_Renderer* renderer, int w, int h, int end_x, int end_y, std::shared_ptr<Player> player) {
+	int start_x = player->getX(), start_y = player->getY();
+
+	// Handle player variables for shot
+	int diff_x = start_x - end_x;
+	int diff_y = start_y - end_y;
+	player->shoot(diff_x, diff_y);
+
+	// Figure out where to start bullet relative to player position and direction
+	if (player->getDir() == Player::left) {
+		start_y += player->getH() / 2;
+	}
+	else if (player->getDir() == Player::right) {
+		start_x += player->getW();
+		start_y += player->getH() / 2;
+	}
+	else if (player->getDir() == Player::up) {
+		start_x += player->getW() / 2;
+	}
+	else if (player->getDir() == Player::down) {
+		start_x += player->getW() / 2;
+		start_y += player->getH();
+	}
+
+	// Create the bullet
+	std::shared_ptr<Bullet> b(new Bullet(renderer, w, h, (float)start_x, (float)start_y, (float)end_x, (float)end_y, 0, 0, 0, 0));
+	_bullets.push_back(b);
+}
+
 void Projectile::draw(SDL_Renderer* renderer) {
 	for (size_t i = 0; i < _targets.size(); i++) 
 		_targets.at(i)->draw(renderer);
@@ -98,98 +192,4 @@ void Projectile::pollEvents(SDL_Event& event) {
 	if (event.type == SDL_MOUSEBUTTONUP) {
 		_mouse_down = false;
 	}
-}
-
-void Projectile::createProjectile(SDL_Renderer* renderer, int w, int h, int end_x, int end_y, std::shared_ptr<Player> player) {
-	int start_x = player->getX(), start_y = player->getY();
-
-	// Handle player variables for shot
-	int diff_x = start_x - end_x;
-	int diff_y = start_y - end_y;
-	player->shoot(diff_x, diff_y);
-
-	// Figure out where to start bullet relative to player position and direction
-	if (player->getDir() == Player::left) {
-		start_y += player->getH() / 2;
-	}
-	else if (player->getDir() == Player::right) {
-		start_x += player->getW();
-		start_y += player->getH() / 2;
-	}
-	else if (player->getDir() == Player::up) {
-		start_x += player->getW() / 2;
-	}
-	else if (player->getDir() == Player::down) {
-		start_x += player->getW() / 2;
-		start_y += player->getH();
-	}
-
-	// Create the bullet
-	std::shared_ptr<Bullet> b(new Bullet(renderer, w, h, (float) start_x, (float) start_y, (float) end_x, (float) end_y, 0, 0, 0, 0));
-	_bullets.push_back(b);
-}
-
-std::map<int, points> Projectile::setupTargetLocations() {
-	std::map<int, points> locations;
-	points p;
-
-	p.point1 = std::make_pair(0, 30);
-	p.point2 = std::make_pair(700, 30);
-	locations[0] = p;
-
-	p.point1 = std::make_pair(0, 100);
-	p.point2 = std::make_pair(0, 200);
-	locations[1] = p;
-
-	p.point1 = std::make_pair(100, 100);
-	p.point2 = std::make_pair(100, 200);
-	locations[2] = p;
-
-	p.point1 = std::make_pair(200, 130);
-	p.point2 = std::make_pair(570, 130);
-	locations[3] = p;
-
-	p.point1 = std::make_pair(200, 230);
-	p.point2 = std::make_pair(570, 230);
-	locations[4] = p;
-
-	p.point1 = std::make_pair(650, 100);
-	p.point2 = std::make_pair(650, 200);
-	locations[5] = p;
-
-	p.point1 = std::make_pair(750, 100);
-	p.point2 = std::make_pair(750, 200);
-	locations[6] = p;
-	return locations;
-}
-
-std::map<int, std::pair<int, int>> Projectile::setupObstLocations() {
-	std::map<int, std::pair<int, int> > locations;
-	locations[0] = std::make_pair(50, 300);
-	locations[1] = std::make_pair(375, 325);
-	locations[2] = std::make_pair(550, 300);
-	locations[3] = std::make_pair(200, 350);
-	locations[4] = std::make_pair(700, 325);
-	locations[5] = std::make_pair(400, 400);
-	locations[6] = std::make_pair(600, 400);
-	locations[7] = std::make_pair(175, 400);
-	return locations;
-}
-
-std::vector<std::shared_ptr<Target>> Projectile::createTargets(SDL_Renderer* renderer) {
-	std::vector<std::shared_ptr<Target>> targets;
-	for (size_t i = 0; i < _target_locations.size(); i++) {
-		std::shared_ptr<Target> target(new Target(renderer, TARGET_SIZE, TARGET_SIZE, _target_locations[i].point1.first, _target_locations[i].point1.second, _target_locations[i].point2.first, _target_locations[i].point2.second, TARGET_FILE_PATH));
-		targets.push_back(target);
-	}
-	return targets;
-}
-
-std::vector<std::shared_ptr<Rect>> Projectile::createObstacles(SDL_Renderer* renderer) {
-	std::vector<std::shared_ptr<Rect>> obstacles;
-	for (size_t i = 0; i < _obst_locations.size(); i++) {
-		std::shared_ptr<Rect> obstacle(new Rect(renderer, ROCK_W, ROCK_H, _obst_locations[i].first, _obst_locations[i].second, ROCK_FILE_PATH));
-		obstacles.push_back(obstacle);
-	}
-	return obstacles;
 }
